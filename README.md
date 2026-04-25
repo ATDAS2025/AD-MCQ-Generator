d3686de419695666933fd36c69d974b110b33b14
Ashutosh Das <ashutoshdas75@gmail.com>
2026-04-25 18:00:40 +0000

    Completion-Research-ipynb

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # AD-MCQ-Generator
-AD-MCQ-Generator
+## AD-MCQ-Generator
+
+1. Create an environment
+```bash
+python -m venv adquizmaker
+
+. adquizmaker/Scripts/Activate
+```
+
+2. Install Requirements
+
+```bash
+pip install -r requirements.txt
+```
+
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AD_MCQ_Generator",
+    version="0.1.0",
+    author="Ashutosh Das",
+    author_email="ashutoshdas75@gmail.com",
+    description="AD'd MCQ Generator",
+    packages=find_packages(),
+    install_requires=[]
+)
